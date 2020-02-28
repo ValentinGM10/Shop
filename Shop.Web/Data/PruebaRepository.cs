c96f8247f1744810b7d5b085dd268117f914bd4e
vgalvez <vgalvez@SH-ET-28>
2020-02-28 12:15:38 -0600

    Implementando el Reositorio

---
+++ Shop.Web/Data/PruebaRepository.cs
@@ -0,0 +1,51 @@
+﻿using Shop.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Data
+{
+    public class PruebaRepository : IRepository
+    {
+        public void AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            var products = new List<Product>();
+            products.Add(new Product { Id = 1, Name = "Uno", Price = 10 });
+            products.Add(new Product { Id = 2, Name = "Dos", Price = 20 });
+            products.Add(new Product { Id = 3, Name = "Tres", Price = 30 });
+            products.Add(new Product { Id = 4, Name = "Cuatro", Price = 40 });
+            return products;
+        }
+
+        public bool ProductExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
